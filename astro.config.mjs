c21ba3f00295f472f574599dabefae098f238c28
Laura Caroline Ribeiro de Melo <laura.c.r.melo@gmail.com>
2021-06-12 23:42:12 +0200

    Add error scenario for tests

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 export default {
-  renderers: ['@astrojs/renderer-preact'],
+  renderers: ['@astrojs/renderer-preact', '@astrojs/renderer-vue'],
   buildOptions: { site: 'http://www.laura-melo/astro-docs' },
   devOptions: {
     tailwindConfig: './tailwindConfig.js',
---
+++ src/components/website/TheHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <p>Test The Header Component!</p>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'TheHeader',
+});
+</script>
+
+<style scoped></style>
