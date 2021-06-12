7e24f46217b3703842d10c4c93010832e04a4eb4
Laura Caroline Ribeiro de Melo <laura.c.r.melo@gmail.com>
2021-06-13 00:34:43 +0200

    Fix scenario and add test instructions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
+/* to run the test properly, after the first npm install 
+add the renderer for vue ('@astrojs/renderer-vue') to the renderers list, 
+and then re run the app (you can also run npm install again and it should still have the same effect) */
+
 export default {
-  renderers: ['@astrojs/renderer-preact', '@astrojs/renderer-vue'],
+  renderers: ['@astrojs/renderer-preact'],
   buildOptions: { site: 'http://www.laura-melo/astro-docs' },
   devOptions: {
     tailwindConfig: './tailwindConfig.js',
