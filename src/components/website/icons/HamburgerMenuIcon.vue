bba593b9598af6edf3123d1a00025686eabfd766
Laura Caroline Ribeiro de Melo <laura.c.r.melo@gmail.com>
2021-06-13 12:02:37 +0200

    Add Burger Menu Icon

---
+++ src/components/website/icons/HamburgerMenuIcon.vue
@@ -0,0 +1,45 @@
+<template>
+  <svg
+    id="Ebene_1"
+    data-name="Ebene 1"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 22 18"
+  >
+    <defs>
+      <style>
+        .cls-1 {
+          fill: none;
+        }
+
+        .cls-2 {
+          fill: currentColor;
+        }
+      </style>
+    </defs>
+    <g id="Menu">
+      <rect
+        id="Rechteck_175"
+        data-name="Rechteck 175"
+        class="cls-1"
+        width="18"
+        height="18"
+      />
+      <path
+        id="Vereinigungsmenge_4"
+        data-name="Vereinigungsmenge 4"
+        class="cls-2"
+        d="M0,17V14.7H22V17Zm0-6.9V7.9H22v2.2ZM0,3.3V1H22V3.3Z"
+      />
+    </g>
+  </svg>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HamburgerMenuIcon',
+});
+</script>
+
+<style scoped></style>
