72a228338e12076638fabc7924a5a22674d465ce
Laura Caroline Ribeiro de Melo <laura.c.r.melo@gmail.com>
2021-06-13 12:03:48 +0200

    Add initial Header to test vue component in astro

---
+++ src/components/website/TheHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav class="navbar">
+    <div>Test Header</div>
+  </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'TheHeader',
+});
+</script>
+
+<style scoped></style>
